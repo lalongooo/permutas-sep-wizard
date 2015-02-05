22a562e0f9c89cdb68217384459593f1512b53a9
Jorge Lalo <hdez.jeduardo@gmail.com>
2015-02-05 00:28:51 -0600

    First commit

---
+++ src/com/example/android/wizardpager/PermutaSepWizardModel.java
@@ -0,0 +1,41 @@
+package com.example.android.wizardpager;
+
+import android.content.Context;
+
+import com.example.android.wizardpager.wizard.model.AbstractWizardModel;
+import com.example.android.wizardpager.wizard.model.BranchPage;
+import com.example.android.wizardpager.wizard.model.PageList;
+import com.example.android.wizardpager.wizard.model.ProfessorInfoPage;
+import com.example.android.wizardpager.wizard.model.SingleFixedChoicePage;
+
+public class PermutaSepWizardModel extends AbstractWizardModel {
+	public PermutaSepWizardModel(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected PageList onNewRootPageList() {
+		return new PageList(new BranchPage(this, "Tipo de plaza")
+				.addBranch(
+						"Estatal",
+						new SingleFixedChoicePage(this, "Tipo de jornada").setChoices(
+								"Jornada Regular", "Jornada ampliada", "Tiempo completo")
+								.setRequired(true),
+						new SingleFixedChoicePage(this, "Carrera magisterial").setChoices(
+								"Si", "No")
+								.setRequired(true))
+
+				.addBranch(
+						"Federal",
+						new SingleFixedChoicePage(this, "Tipo de jornada").setChoices(
+								"Jornada Regular", "Jornada ampliada", "Tiempo completo")
+								.setRequired(true),
+						new SingleFixedChoicePage(this, "Carrera magisterial").setChoices(
+								"Si", "No")
+								.setRequired(true))
+
+				.setRequired(true),
+
+		new ProfessorInfoPage(this, "Tu información").setRequired(true));
+	}
+}
---
+++ src/com/example/android/wizardpager/wizard/model/ProfessorInfoPage.java
@@ -0,0 +1,38 @@
+package com.example.android.wizardpager.wizard.model;
+
+import com.example.android.wizardpager.wizard.ui.CustomerInfoFragment;
+
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+
+/**
+ * A page asking for a name and an email.
+ */
+public class ProfessorInfoPage extends Page {
+	public static final String NAME_DATA_KEY = "name";
+	public static final String EMAIL_DATA_KEY = "email";
+	public static final String MOBILE_DATA_KEY = "mobile";
+
+	public ProfessorInfoPage(ModelCallbacks callbacks, String title) {
+		super(callbacks, title);
+	}
+
+	@Override
+	public Fragment createFragment() {
+		return CustomerInfoFragment.create(getKey());
+	}
+
+	@Override
+	public void getReviewItems(ArrayList<ReviewItem> dest) {
+		dest.add(new ReviewItem("Tu nombre", mData.getString(NAME_DATA_KEY), getKey(), -1));
+		dest.add(new ReviewItem("Tu correo", mData.getString(EMAIL_DATA_KEY), getKey(), -1));
+		dest.add(new ReviewItem("Tu teléfono", mData.getString(MOBILE_DATA_KEY), getKey(), -1));
+	}
+
+	@Override
+	public boolean isCompleted() {
+		return !TextUtils.isEmpty(mData.getString(NAME_DATA_KEY));
+	}
+}
