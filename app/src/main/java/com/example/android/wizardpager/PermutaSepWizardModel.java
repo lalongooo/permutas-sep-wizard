134e5561bef86ca65dd9ae90787b588fc7580939
Jorge Lalo <hdez.jeduardo@gmail.com>
2015-03-02 20:14:19 -0600

    Added constant values for the page keys in the wizard model

--- app/src/main/java/com/example/android/wizardpager/PermutaSepWizardModel.java
+++ app/src/main/java/com/example/android/wizardpager/PermutaSepWizardModel.java
@@ -14,15 +14,22 @@ public class PermutaSepWizardModel extends AbstractWizardModel {
 		super(context);
 	}
 
+    public static final String CONTACT_INFO_KEY = "contact_info_page_key";
+    public static final String CITY_FROM_KEY = "city_from_page_key";
+    public static final String CITY_TO_KEY = "city_to_page_key";
+    public static final String POSITION_TYPE_KEY = "position_type_page_key";
+    public static final String WORKDAY_TYPE_KEY = "workday_type_page_key";
+    public static final String TEACHING_CAREER_KEY = "teaching_career_page_key";
+
 	@Override
 	protected PageList onNewRootPageList() {
 		return new PageList(
-				new ProfessorContactInfoPage(this, "Tu información").setRequired(true).setKey("contact_info"),
-				new ProfessorCityFromPage(this, "Tu ciudad de origen").setRequired(true).setKey("city_from"),
-				new ProfessorCityToPage(this, "Tu lugar deseado").setRequired(true).setKey("desired_city"),
-				new SingleFixedChoicePage(this, "Tipo de plaza").setChoices("Estatal", "Federal").setRequired(true).setKey("type_of_position"),
-				new SingleFixedChoicePage(this, "Tipo de jornada").setChoices("Jornada Regular", "Jornada ampliada", "Tiempo completo").setRequired(true).setKey("schedule_type"),
-				new SingleFixedChoicePage(this, "Carrera magisterial").setChoices("Si", "No").setRequired(true).setKey("magisterium_career")
+                new ProfessorContactInfoPage(this, "Tu información").setRequired(true).setKey(CONTACT_INFO_KEY),
+                new ProfessorCityFromPage(this, "Tu ciudad de origen").setRequired(true).setKey(CITY_FROM_KEY),
+                new ProfessorCityToPage(this, "Tu lugar deseado").setRequired(true).setKey(CITY_TO_KEY),
+                new SingleFixedChoicePage(this, "Tipo de plaza").setChoices("Estatal", "Federal").setRequired(true).setKey(POSITION_TYPE_KEY),
+                new SingleFixedChoicePage(this, "Tipo de jornada").setChoices("Jornada Regular", "Jornada ampliada", "Tiempo completo").setRequired(true).setKey(WORKDAY_TYPE_KEY),
+                new SingleFixedChoicePage(this, "Carrera magisterial").setChoices("Si", "No").setRequired(true).setKey(TEACHING_CAREER_KEY)
 				);
 	}
 }
