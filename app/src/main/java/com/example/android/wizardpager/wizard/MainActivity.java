5539fffce033e5f083b350184c4a5733e314a60d
lalongooo <hdez.jeduardo@gmail.com>
2015-03-11 17:55:01 -0600

    Moved to Fragment (new files)

---
+++ app/src/main/java/com/example/android/wizardpager/wizard/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.wizardpager.wizard;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.example.android.wizardpager.R;
+
+/**
+ * Created by jorge.hernandez on 3/11/2015.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
